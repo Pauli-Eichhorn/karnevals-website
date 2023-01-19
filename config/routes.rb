e3581ab36ed9b637bde352861ab8c94af0be459f
Paul-Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2023-01-19 19:41:14 +0100

    2 extra pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   get "disco", to: "pages#disco", as: :disco
+  get "dress", to: "pages#dress", as: :dress
+  get "english", to: "pages#english", as: :english
 end
